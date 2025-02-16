905de5ef4f73e7739e19a9b46a847dbff70e1701
Spencer Smith <spenceralansmith@icloud.com>
2025-02-16 14:26:26 -0800

    ship now rotates

--- main.py
+++ main.py
@@ -2,21 +2,27 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-from constants import *
+from constants import (SCREEN_WIDTH, SCREEN_HEIGHT)
+from player import Player
 
 
 def main():
-    print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
-    screen = pygame.display.set_mode((SCREEN_HEIGHT, SCREEN_WIDTH))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0, 0, 0))
-    pygame.display.flip()
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
